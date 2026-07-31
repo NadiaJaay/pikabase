532b243899dd3d7039de3837533a1e31021aaf64
Nadia JujoosteE <yourgitemail@example.com>
2026-07-30 20:27:34 -0400

    initial commit

---
+++ src/main/java/com/nadiajaay/pika_base/PikaBaseApplication.java
@@ -0,0 +1,13 @@
+package com.nadiajaay.pika_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PikaBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PikaBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nadiajaay/pika_base/PikaBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadiajaay.pika_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PikaBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
